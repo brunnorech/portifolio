a242cfa3d8208868cf89d970fd17d4882851c55d
Bruno Rech <brunno.rech@outlook.com>
2025-03-15 23:29:36 -0300

    feat: test api

---
+++ src/app/api/restart/route.ts
@@ -0,0 +1,18 @@
+import { exec } from 'child_process';
+
+export async function GET() {
+  return new Promise((resolve, reject) => {
+    exec('pm2 restart next-app', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Erro ao reiniciar o servidor: ${error.message}`);
+        return reject(new Response(`Erro: ${error.message}`, { status: 500 }));
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return reject(new Response(stderr, { status: 500 }));
+      }
+      console.log(`Servidor reiniciado com sucesso: ${stdout}`);
+      return resolve(new Response(`Servidor reiniciado com sucesso: ${stdout}`, { status: 200 }));
+    });
+  });
+}
