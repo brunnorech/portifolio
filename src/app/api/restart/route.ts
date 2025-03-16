5aba5f0a7921cdb89247ac177154ec4706be1373
Bruno Rech <brunno.rech@outlook.com>
2025-03-15 23:33:40 -0300

    fix: build

--- src/app/api/restart/route.ts
+++ src/app/api/restart/route.ts
@@ -1,7 +1,7 @@
 import { exec } from 'child_process';
 
 export async function GET() {
-  return new Promise((resolve, reject) => {
+  return new Promise<void | Response>((resolve, reject) => {
     exec('pm2 restart next-app', (error, stdout, stderr) => {
       if (error) {
         console.error(`Erro ao reiniciar o servidor: ${error.message}`);
